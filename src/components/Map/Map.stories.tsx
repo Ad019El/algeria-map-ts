22f193e289afded8e0c7df5ce8b45a5cb8f1ffe0
Ad019El <a.dafi@esi-sba.dz>
2023-12-30 17:23:48 +0100

    added multiple color map

--- src/components/Map/Map.stories.tsx
+++ src/components/Map/Map.stories.tsx
@@ -11,14 +11,49 @@ const meta: Meta<typeof Map> = {
 export default meta;
 type Story = StoryObj<typeof Map>;
 
-const data = jsonData;
-console.log(data);
+const assignColor = (value: number) => {
+  if (value <= 50) {
+    return "#fff9eb";
+  } else if (value <= 100) {
+    return "#ebf9ee";
+  } else if (value <= 250) {
+    return "#cef0d7";
+  } else if (value <= 500) {
+    return "#a7e4b7";
+  } else if (value <= 1000) {
+    return "#7ed896";
+  } else if (value <= 5000) {
+    return "#57cc76";
+  } else {
+    return "#32c158";
+  }
+};
+
+// add color rage for each wilaya and add the color to the map
+const multipleColor = Object.entries(jsonData).reduce<{ [key: string]: any }>(
+  (acc, [wilaya, value]) => {
+    acc[wilaya] = { ...value, color: assignColor(value.value) };
+    return acc;
+  },
+  {}
+);
 
 export const OneColor: Story = {
   render: () => (
     <Map
-      data={data}
+      data={jsonData}
       color="#2ab7ca"
+      HoverColor="#f4f4f8"
+      stroke="#000000"
+      hoverStroke="#000000"
+    />
+  ),
+};
+
+export const MultipleColor: Story = {
+  render: () => (
+    <Map
+      data={multipleColor}
       HoverColor="#f4f4f8"
       stroke="#000000"
       hoverStroke="#000000"
