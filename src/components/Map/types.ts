5b7e965f35f63876fd424350e8f2e359f6574686
AbdeldjabarDev <ousseddikabdo@gmail.com>
2024-08-19 11:22:30 +0100

    Added dynamic typing to MapProps

--- src/components/Map/types.ts
+++ src/components/Map/types.ts
@@ -1,13 +1,17 @@
-export interface IMapProps {
+export type  DefaultType = {
+  value : number,
+  color? : string
+}
+export interface IMapProps<T = DefaultType> {
   height?: string;
   width?: string;
   color?: string;
   stroke?: string;
   hoverStroke?: string;
   HoverColor?: string;
-  data: { [key: string]: { value: number | string; color?: string } };
-  onWilayaClick?: (wilaya: string, value: number | string) => void;
-  getHoverContent?: (record : any) => string;
+  data: { [key: string]: T };
+  onWilayaClick?: (wilaya: string, value: DefaultType) => void;
+  getHoverContent?: (record : DefaultType) => string;
   hoverContentStyle? : React.CSSProperties,
   getHoverContentStyle? : (record : any) => React.CSSProperties
 }
